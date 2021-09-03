3f8f46aa27fbfcbb205868408c445d9081054292
Gustavo Pereira <pereiragustavo215@gmail.com>
2021-09-02 22:20:06 -0300

    Implementacao uploadImage e createProduct

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -5,8 +5,10 @@ import br.com.douglasmotta.whitelabeltutorial.BuildConfig
 import br.com.douglasmotta.whitelabeltutorial.domain.modal.Product
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -40,10 +42,37 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childReference = storeReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey")
+
+            childReference.putFile(imageUri)
+                .addOnSuccessListener { taskIsSnapshot ->
+                    taskIsSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        val path = uri.toString()
+                        continuation.resumeWith(Result.success(path))
+                    }
+
+                }.addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
+
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            documentReference
+                .collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+
+                }.addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
+        }
     }
 }
