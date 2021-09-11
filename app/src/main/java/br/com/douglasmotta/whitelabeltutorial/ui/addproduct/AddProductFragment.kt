57aa6dae1089af5b99f7b820e0325a452da1ec7c
Gustavo Pereira <pereiragustavo215@gmail.com>
2021-09-11 12:26:39 -0300

    aad listeners and getUri from OS

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
@@ -1,16 +1,26 @@
 package br.com.douglasmotta.whitelabeltutorial.ui.addproduct
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.douglasmotta.whitelabeltutorial.databinding.AddProductFragmentBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: AddProductFragmentBinding? = null
     private val binding get() = _binding!!
+    private var imageUri: Uri? =null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        imageUri = uri
+        binding.imageProduct.setImageURI(uri)
+    }
+
     private lateinit var viewModel: AddProductViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -18,4 +28,28 @@ class AddProductFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val prince = binding.inputPrice.text.toString()
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage(){
+        getContent.launch("image/*")
+    }
 }
